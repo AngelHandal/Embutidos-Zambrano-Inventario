01478e4a9294098ca7e8d54738ec49d4e7eeaec6
AngelHandal <anhehandal@gmail.com>
2023-02-17 21:49:32 -0600

    Creacion de usuarios, se modificaron las vistas, y se aplico codigo javascript para temas de seguridad de la creacion de usuarios

--- ZambranoInventory/ZambranoAPP/urls.py
+++ ZambranoInventory/ZambranoAPP/urls.py
@@ -8,5 +8,6 @@ urlpatterns = [
     path('cerrar', views.cerrar_sesion, name='cerrar_sesion'),
     path('user_views', views.users_view, name='user_views'),
     path('roles_views', views.rols_view, name='rols_views'),
+    path('crear_usuario', views.crear_usuario, name='crear_usuarios'),
 
 ]
--- ZambranoInventory/ZambranoAPP/views.py
+++ ZambranoInventory/ZambranoAPP/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect
 from django.contrib import messages
-from .models import TblMsUsuario, TblMsRoles
+from .models import TblMsUsuario, TblMsRoles, TblMsPreguntas
+from django.utils import timezone
 
 
 def bienvenido(request):
@@ -84,5 +85,39 @@ def cerrar_sesion(request):
             return redirect('login')
     # Si la petición no es un POST o no hay usuario en la sesión, redirige al usuario a la página de bienvenida    
     return redirect('bienvenido')
+
+
+def crear_usuario(request):
+    
+    roles = TblMsRoles.objects.all()
+    preguntas = TblMsPreguntas.objects.all()
+
+    if request.method == "POST":
+        rol_id = request.POST.get("rol")
+        rol = TblMsRoles.objects.get(pk=rol_id)
+        ultimo_id = TblMsUsuario.objects.order_by("-id_usuario").first().id_usuario
+        nuevo_id = ultimo_id + 1
+        usuario = TblMsUsuario(
+            id_usuario=nuevo_id,
+            usuario=request.POST.get("usuario"),
+            nombre_usuario = request.POST.get("nombre_usuario"),
+            estado_usuario = request.POST.get("estado_usuario"),
+            contrasena=request.POST.get("contrasena"),
+            correo_electronico=request.POST.get("correo_electronico"),
+            id_rol=rol,
+            preguntas_contestadas='1',
+            # respuesta=request.POST.get("respuesta"),
+            creado_por="usuario",
+            modificado_por="usuario",
+            fecha_ultima_conexion=timezone.now(), # Añadimos la fecha de última conexión
+            primer_ingreso = "1",
+            fecha_vencimiento = request.POST.get("fecha_vencimiento"),
+            fecha_creacion = timezone.now(),
+            fecha_modificacion = timezone.now(),
+        )
+        usuario.save()
+        messages.success(request, "Usuario creado con éxito.")
+        return redirect("bienvenido")
+    return render(request, "crear_usuario.html", {"roles": roles, "preguntas": preguntas})
 
 
